f9fc9ea5efdb00d0698d5dc95006209215801f99
Edgar Lopez <edgarylop@gmail.com>
2016-12-01 14:28:29 -0800

    Added gameboard implementation

--- App.jsx
+++ App.jsx
@@ -7,11 +7,20 @@ import BoardCell from './BoardCell.jsx';
 function App(props){
   // Game logic here (use seperate game logic components)
   // UI created + control here
-  var boardCells = [<BoardCell key='1' markType='X' />, <BoardCell key='2' markType='O' />, <BoardCell key='3' markType=' ' />];
+  var boardCells = [<BoardCell key='1' markType='X' cellStyle='top-left' />,
+                    <BoardCell key='2' markType='O' cellStyle='top-center' />,
+                    <BoardCell key='3' markType=' ' cellStyle='top-right' />,
+                    <div></div>,
+                    <BoardCell key='4' markType=' ' cellStyle='middle-left' />,
+                    <BoardCell key='5' markType=' ' cellStyle='middle-center' />,
+                    <BoardCell key='6' markType=' ' cellStyle='middle-right' />,
+                    <div></div>,
+                    <BoardCell key='7' markType=' ' cellStyle='bottom-left' />,
+                    <BoardCell key='8' markType=' ' cellStyle='bottom-center' />,
+                    <BoardCell key='9' markType=' ' cellStyle='bottom-right' />];
   
   return ( <div>
       <PlayerTurn player={props.playerTurn} />
-      <div>The game board</div>
       <Gameboard cellElements={boardCells} />
       <Scoreboard xwins={props.xwins} owins={props.owins} />
     </div>
--- BoardCell.jsx
+++ BoardCell.jsx
@@ -8,13 +8,91 @@ function BoardCell(props){
     display: 'inline-block',
     width: '100px',
     textAlign: 'center',
-    border: '1px solid black',
     whiteSpace: 'pre'
   };
   
-  return (<div style={style}>
+  var applyStyle = Object.assign({}, style);
+  applyStyle = Object.assign(applyStyle, chooseStyle(props.cellStyle));
+  
+  return (<div style={applyStyle}>
       <h1>{props.markType}</h1>
     </div>);
+}
+
+function chooseStyle(styleString){
+  var topLeftStyle = {
+    borderBottom: '1px solid black',
+    borderRight: '1px solid black'
+  };
+  
+  var topCenterStyle = {
+    borderBottom: '1px solid black',
+    borderRight: '1px solid black',
+    borderLeft: '1px solid black'
+  };
+  
+  var topRightStyle = {
+    borderBottom: '1px solid black',
+    borderLeft: '1px solid black'
+  };
+  
+  var middleLeftStyle = {
+    borderTop: '1px solid black',
+    borderBottom: '1px solid black',
+    borderRight: '1px solid black',
+  };
+  
+  var middleCenterStyle = {
+    borderTop: '1px solid black',
+    borderBottom: '1px solid black',
+    borderRight: '1px solid black',
+    borderLeft: '1px solid black'
+  };
+  
+  var middleRightStyle = {
+    borderTop: '1px solid black',
+    borderLeft: '1px solid black',
+    borderBottom: '1px solid black'
+  };
+  
+  var bottomLeftStyle = {
+    borderTop: '1px solid black',
+    borderRight: '1px solid black'
+  };
+  
+  var bottomCenterStyle = {
+    borderTop: '1px solid black',
+    borderLeft: '1px solid black',
+    borderRight: '1px solid black'
+  };
+  
+  var bottomRightStyle = {
+    borderTop: '1px solid black',
+    borderLeft: '1px solid black'
+  };
+  
+  if( styleString === 'top-left' ){
+    return topLeftStyle;
+  }else if( styleString === 'top-center' ){
+    return topCenterStyle;
+  }else if( styleString === 'top-right' ){
+    return topRightStyle;
+  }else if( styleString === 'middle-left' ){
+    return middleLeftStyle;
+  }else if( styleString === 'middle-center' ){
+    return middleCenterStyle;
+  }else if( styleString === 'middle-right' ){
+    return middleRightStyle;
+  }else if( styleString === 'bottom-left' ){
+    return bottomLeftStyle;
+  }else if( styleString === 'bottom-center' ){
+    return bottomCenterStyle;
+  }else if( styleString === 'bottom-right' ){
+    return bottomRightStyle;
+  }else{
+    return {};
+  }
+  
 }
 
 export default BoardCell;
